eaf915f8239377513bc8df6dfa32038038fad937
mino2357 <mino2357@gmail.com>
2018-04-05 05:05:46 +0900

    fix

--- Head-eq/app/Main.hs
+++ Head-eq/app/Main.hs
@@ -7,6 +7,7 @@ import Data.Array.Repa (Z(..), (:.)(..))
 import qualified Data.Array.Repa as R
 import Data.Array.Repa.Stencil as R
 import Data.Array.Repa.Stencil.Dim2 as R
+import Data.Array.Repa.Slice as R
 
 -- Unboxed Array Type
 type Vector1dU = R.Array R.U R.DIM1 Double
@@ -25,7 +26,7 @@ xMax :: (Floating a) => a
 xMax = pi
 -- 時間刻み
 dt :: (Floating a) => a
-dt = 0.001
+dt = 0.01
 -- 拡散係数
 d :: (Floating a) => a
 d = 1.0
@@ -70,17 +71,51 @@ u :: Vector1dU
 u = makeInitCondition sin xMin xMax nDiv
 
 
+--timeDev :: Vector1dU -> Vector1dD
+--timeDev u =
+timeDevSub :: Vector1dU -> Vector1dD
+timeDevSub u =
+  dr >< u1 R.+^ (1.0 - 2.0 * dr) >< u2 R.+^ dr >< u3
+  where
+    zero = R.fromListUnboxed (Z :. 1) [0.0]
+    u1 = u    R.++ zero R.++ zero
+    u2 = zero R.++ u    R.++ zero
+    u3 = zero R.++ zero R.++ u
+    -- Scalar multiplication in Repa
+    infixl 7 ><
+    (><) x v = R.map (* x) v
+
 timeDev :: Vector1dU -> Vector1dD
-timeDev u1 = 
-    dr >< u1 R.+^ (1 - 2 * dr) >< u2 R.+^ dr >< u3
-    where
-        zero = R.fromListUnboxed (Z :. 1) [0.0]
-        u2 = zero R.++ u1
-        u3 =  zero R.++ u2
-        -- Scalar multiplication in Repa
-        infixl 7 ><
-        (><) x v = R.map (* x) v
+timeDev u = zero R.++ (R.extract (Z :. 2) (Z :.(nDiv-1)) $ timeDevSub u) R.++ zero
+  where
+    zero = R.fromListUnboxed (Z :. 1) [0.0]
+
+--debug :: Vector1dU -> Vector1dD
+--debug u = 
+--  dr >< u1 R.+^ (1.0 - 2.0 * dr) >< u2 R.+^ dr >< u3
+--  where
+--    zero = R.fromListUnboxed (Z :. 1) [0.0]
+--    u1 = u    R.++ zero R.++ zero
+--    u2 = zero R.++ u    R.++ zero
+--    u3 = zero R.++ zero R.++ u
+--    -- Scalar multiplication in Repa
+--    infixl 7 ><
+--    (><) x v = R.map (* x) v
 
 main :: IO ()
 main = do
+  print $ makeInitCondition sin xMin xMax nDiv
+  print $ R.computeUnboxedS $ timeDevSub u
   print $ R.computeUnboxedS $ timeDev u
+
+
+
+
+
+
+
+
+
+
+
+
